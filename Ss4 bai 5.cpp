bc1ff8bacef26d35961d01b0e02328c3c647b389
Nguyen Duy Manh <manhduy0989@gmail.com>
2024-11-25 09:33:31 +0700

    Update Ss4 bai 5.cpp

--- Ss4 bai 5.cpp
+++ Ss4 bai 5.cpp
@@ -7,7 +7,7 @@ int main(){
 	scanf("%d",&num2);
 	printf("Nhap vao so nguyen thu ba: ");
 	scanf("%d",&num3);
-	if (num1<=num3<=num2 || num1>=num3>=num2) 
+	if (num1<=num3&&num3<=num2 || num1>=num3&&num3>=num2) 
 	printf("So %d nam giua so %d va so %d", num3, num1, num2);
 	else printf("So %d khong nam giua so %d va so %d", num3, num1, num2);
 
